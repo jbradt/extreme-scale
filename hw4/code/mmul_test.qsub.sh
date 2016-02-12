de165663ca325f3c953bd91aa42e6cfcac87c073
Joshua Bradt <bradt@nscl.msu.edu>
2016-02-12 14:24:53 -0500

    Updated makefile, changed job script.

--- hw4/code/mmul_test.qsub.sh
+++ hw4/code/mmul_test.qsub.sh
@@ -2,26 +2,23 @@
 #PBS -N mmul
 #PBS -j oe
 #PBS -o /mnt/home/jbradt/Documents/Code/extreme-scale/hw4/outputs/output.o
-#PBS -l walltime=00:15:00
+#PBS -l walltime=01:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -l feature=intel14
-#PBS -l mem=100mb
+#PBS -l mem=250mb
 #PBS -m a
 #PBS -M jbradt@msu.edu
+#PBS -t 0,1,2,3
 
 cd /mnt/home/jbradt/Documents/Code/extreme-scale/hw4/code
 
-OLEVELS=("0" "1" "2" "3")
 DIMS=("20" "100" "200" "800" "1000" "1200" "1600" "1800" "2000")
 
-for OLEVEL in "${OLEVELS[@]}"
+echo "Optimization level O${PBS_ARRAYID}"
+
+for DIM in "${DIMS[@]}"
 do
-    echo "Optimization level O$OLEVEL"
+    echo "For N=$DIM"
 
-    for DIM in "${DIMS[@]}"
-    do
-        echo "For N=$DIM"
-
-        ./mmul${OLEVEL} ${DIM}
-    done
+    ./mmul${PBS_ARRAYID} ${DIM}
 done
