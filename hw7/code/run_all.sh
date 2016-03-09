ffdc2a156da846eaa26051f84575f7a519a08563
Joshua Bradt <bradt@nscl.msu.edu>
2016-03-09 17:05:25 -0500

    Updated run_all script to work for qsub.

--- hw7/code/run_all.sh
+++ hw7/code/run_all.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+#PBS -N mmul-omp
+#PBS -j oe
+#PBS -o /mnt/home/jbradt/Documents/Code/extreme-scale/hw7/outputs/output.o
+#PBS -l walltime=01:30:00
+#PBS -l nodes=1:ppn=20
+#PBS -l feature=intel14
+#PBS -l mem=500mb
+#PBS -m abe
+#PBS -M jbradt@msu.edu
 
 MMULOMP=./mmul-omp
 MMULNOOMP=./mmul-noomp
 
-SIZES=("20" "100" "1000")
-THREADS=("1" "2" "3" "4" "5" "6" "7" "8")
+SIZES=("20" "100" "200" "800" "1000" "1200" "1600" "1800" "2000")
 
 for SIZE in "${SIZES[@]}"; do
     echo "N=$SIZE TH=0"
     $MMULNOOMP $SIZE
 
-    for THCT in "${THREADS[@]}"; do
+    for THCT in {1..20}; do
         echo "N=$SIZE TH=$THCT"
         export OMP_NUM_THREADS=$THCT
         $MMULOMP $SIZE
