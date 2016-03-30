d8c15aa61757f51ea670a2c4522ce03ca3b4b776
Joshua Bradt <bradt@nscl.msu.edu>
2016-03-30 15:56:05 -0400

    Added script for bw.

---
+++ hw8/code/bw_job.qsub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N comm_test
+#PBS -q normal
+#PBS -A babq
+#PBS -j oe
+#PBS -l walltime=00:02:00
+#PBS -l nodes=32:ppn=32:xe
+#PBS -m abe
+#PBS -M jbradt@msu.edu
+
+source ${HOME}/setenv_gcc.sh
+
+cd ${PBS_O_WORKDIR}
+
+aprun -n 32 -N 1 ./comm_test > output.out
